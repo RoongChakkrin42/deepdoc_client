e298c16e47cceeada7d84e6a05da780b220a2fe8
shin <shinshiro1193@gmail.com>
2025-07-23 16:01:01 +0700

    add features + bug fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import Layout from "../components/layout";
 import { Provider } from "react-redux";
 import { store } from "../store";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>DeepDoc</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
